8c7d3f7123e55caeba85cb38caa465f686854fd8
Rebouh Aymen <aymenworks@gmail.com>
2015-03-21 15:04:35 +0100

    [IMP] Archiving on beacon model

--- StudentProjectInfotel/Managers/LocationManager.swift
+++ StudentProjectInfotel/Managers/LocationManager.swift
@@ -18,13 +18,15 @@ class LocationManager: NSObject, CLLocationManagerDelegate {
     override init() {
         
         super.init()
-        println("init location")
-        locationManager = CLLocationManager()
-        locationManager.delegate = self
+        println("LocationManager init")
+        self.locationManager = CLLocationManager()
+        self.locationManager.delegate = self
         
-        if locationManager.respondsToSelector("requestAlwaysAuthorization") {
+        if self.locationManager.respondsToSelector("requestAlwaysAuthorization") {
             self.locationManager.requestAlwaysAuthorization()
         }
+        
+        self.locationManager.startUpdatingLocation()
     }
     
     // MARK: - Beacon Location -
@@ -33,7 +35,6 @@ class LocationManager: NSObject, CLLocationManagerDelegate {
         self.locationManager.startMonitoringForRegion(beacon.region)
         self.locationManager.startRangingBeaconsInRegion(beacon.region)
         self.locationManager.requestStateForRegion(beacon.region)
-        self.locationManager.startUpdatingLocation()
     }
     
     func stopMonitoringBeacon(beacon: Beacon) {
@@ -42,43 +43,40 @@ class LocationManager: NSObject, CLLocationManagerDelegate {
         self.locationManager.stopUpdatingLocation()
     }
     
-    /**
-    Called when a beacon come within range, move out of range, or when the range of an iBeacon changes.
     
-    :param: manager The location manager object reporting the event.
-    :param: beacons An array of CLBeacon objects representing the beacons currently in range.
-    :param: region  The region object containing the parameters that were used to locate the beacons.
-    */
-    func locationManager(manager: CLLocationManager!,
-                                    didRangeBeacons beacons: [AnyObject]!, inRegion region: CLBeaconRegion!) {
-        var message = ""
-        
-        for clBeacon in beacons as [CLBeacon] {
+    // Called when a beacon come within range, move out of range, or when the range of an iBeacon changes.
+    func locationManager(manager: CLLocationManager!, didRangeBeacons beacons: [AnyObject]!,
+        inRegion region: CLBeaconRegion!) {
             
-            for beacon in Facade.sharedInstance().beacons() {
-                
-                if beacon == clBeacon {
-                    
-                    // If the beacon has already appeared
-                    if let lastProximity = beacon.lastSeenBeacon?.proximity {
-                        // If its proximity havn't changed, we don't update anything
-                        if clBeacon.proximity == lastProximity || clBeacon.proximity == .Unknown {
+            
+        // For each beacon around
+
+        for clBeacon in beacons as [CLBeacon] {
+            for room in Facade.sharedInstance().rooms() {                   // For each beacon from our database
+                if room.beacon == clBeacon {
+                    println("J'ai trouvé des beacons dans les environs et qui m'appartient= \(room.beacon)")
+
+                    // If that's our
+                    if let lastProximity = room.beacon.lastSeenBeacon?.proximity {       // If the beacon has already apparead
+                        if clBeacon.proximity == lastProximity || clBeacon.proximity == .Unknown { // If the beacon proximity hasn't changed
                             return
+                        } else {
+                            println("\(room.beacon) changed proximity")
                         }
                     }
-                    
-                    beacon.lastSeenBeacon = clBeacon
-                    NSNotificationCenter.defaultCenter().postNotificationName("BeaconSendLocalNotification", object: self,
-                        userInfo: ["message": "\(beacon.lastSeenBeacon!.proximity.toString()) \(beacon.name)"])
+                    room.beacon.lastSeenBeacon = clBeacon
+                    println("J'envoie une requête au serveur pour indiquer ma présence avec ce beacon = \(room.beacon)")
+                    Facade.sharedInstance().addMyPresenceToRoom(room.identifier,
+                        userEmail: Member.sharedInstance().email!.encodeBase64(), completionHandler: { (json, error) -> Void in
+                            println("add my presence json = \(json), error = \(error)")
+                    })
                 }
             }
         }
-        
-        println("Beacon message = \(message)")
     }
 
     // MARK: - User Location -
-
+    
     func locationManager(manager: CLLocationManager!, didFailWithError error: NSError!) {
         println("locationManager didFailWithError = \(error)")
     }
@@ -90,35 +88,46 @@ class LocationManager: NSObject, CLLocationManagerDelegate {
     func locationManager(manager: CLLocationManager!, didDetermineState state: CLRegionState, forRegion region: CLRegion!) {
         if state == .Inside {
             println(".Inside")
-        } else {
+        } else if state == .Outside {
             println("Not .Inside")
+            Facade.sharedInstance().deleteMyPresenceFromRoom(Member.sharedInstance().email!.encodeBase64(), completionHandler: { (json, error) -> Void in})
         }
     }
     
-    /* There's no need to start monitoring/scaning if we're not at the area. So we begin to monitore only when 
+    /* There's no need to start monitoring/scaning if we're not at the area. So we begin to monitore only when
         the user enter the region
     */
     func locationManager(manager: CLLocationManager!, didEnterRegion region: CLRegion!) {
-        manager.startRangingBeaconsInRegion(region as CLBeaconRegion)
-        manager.startUpdatingLocation()
+        println("start monitoring/scaning because entered the region")
+
+        self.locationManager.startRangingBeaconsInRegion(region as CLBeaconRegion)
+        self.locationManager.startUpdatingLocation()
         
-        println("You entered the region")
     }
     
     // If we exit the area, there's no need to continue monitoring/scaning
     func locationManager(manager: CLLocationManager!, didExitRegion region: CLRegion!) {
-        manager.stopRangingBeaconsInRegion(region as CLBeaconRegion)
-        manager.stopUpdatingLocation()
+      //  println("stop monitoring/scaning because entered the region")
+      //  self.locationManager.stopRangingBeaconsInRegion(region as CLBeaconRegion)
         
+        Facade.sharedInstance().deleteMyPresenceFromRoom(Member.sharedInstance().email!.encodeBase64(), completionHandler: { (json, error) -> Void in
+            println("json = \(json), error = \(error)")
+        })
+
         println("You exited the region")
     }
     
     // MARK: - Location Authorization -
     
     func locationManager(manager: CLLocationManager!, didChangeAuthorizationStatus status: CLAuthorizationStatus) {
-        
-        if status == .Denied {
-            println("denied")
+        switch(status) {
+            case .Denied:
+                println("denied")
+            case .AuthorizedAlways:
+                println("AuthorizedAlways")
+                self.locationManager.startUpdatingLocation()
+
+            default:break
         }
     }
 }
--- StudentProjectInfotel/Models/Beacon.swift
+++ StudentProjectInfotel/Models/Beacon.swift
@@ -9,13 +9,15 @@
 /**
   The beacon model.
 */
-public class Beacon {
+public class Beacon: NSObject {
     
-    private(set) var name: String
-    private var uuid: NSUUID
-    private var major: CLBeaconMajorValue
-    private var minor: CLBeaconMinorValue
-    private(set) var region: CLBeaconRegion
+    private(set) var name: String! = ""
+    private var uuid: NSUUID!
+    private var major: CLBeaconMajorValue!
+    private var minor: CLBeaconMinorValue!
+    private(set) var region: CLBeaconRegion!
+    
+    /// This property stores the last CLBeacon instance seen for the current beacon. This is used to display the proximity information.
     var lastSeenBeacon: CLBeacon?
     
     // MARK: - Initialization -
@@ -27,6 +29,32 @@ public class Beacon {
         self.minor = minor
         region = CLBeaconRegion(proximityUUID: self.uuid, major: self.major, minor: self.minor, identifier: self.name)
         region.notifyEntryStateOnDisplay = true
+    }
+    
+    required public init(coder aDecoder: NSCoder) {
+        super.init()
+        self.uuid = aDecoder.decodeObjectForKey("uuid") as NSUUID
+        self.major = (aDecoder.decodeObjectForKey("major") as NSNumber).unsignedShortValue
+        self.minor = (aDecoder.decodeObjectForKey("minor") as NSNumber).unsignedShortValue
+        self.name = aDecoder.decodeObjectForKey("name") as String
+        self.region = aDecoder.decodeObjectForKey("region") as CLBeaconRegion
+    }
+}
+
+extension Beacon: NSCoding {
+    
+    public func encodeWithCoder(aCoder: NSCoder) {
+        aCoder.encodeObject(self.uuid, forKey: "uuid")
+        aCoder.encodeObject(NSNumber(unsignedShort: self.major), forKey: "major")
+        aCoder.encodeObject(NSNumber(unsignedShort: self.minor), forKey: "minor")
+        aCoder.encodeObject(self.name, forKey: "name")
+        aCoder.encodeObject(self.region, forKey: "region")
+    }
+}
+
+extension Beacon: Printable {
+    override public var description: String {
+        return "Beacon \(self.name) major = \(self.major), minor = \(self.minor) ranged \(self.lastSeenBeacon?.proximity.toString())."
     }
 }
 
